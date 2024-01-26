dd97ac10e5a5fb7b7b2b8b0c88267ecfd8ea2ff1
evelynguzman <evelabohemia@gmail.com>
2024-01-26 18:06:34 -0300

    cambios en el M06

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,6 +2,10 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+
+  return (num >= 100 && num <= 999) || (num <= -100 && num >= -999);
 }
+
+console.log(tieneTresDigitos(-100));
 
 module.exports = tieneTresDigitos;
